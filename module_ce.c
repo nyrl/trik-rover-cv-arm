c6bc93815eac999ed61f2b7b172e10fb069d6760
Denis Dydychkin <nyrl@mail.ru>
2013-11-01 14:16:29 +0400

    Fixed number of output stream

--- module_ce.c
+++ module_ce.c
@@ -114,7 +114,7 @@ static int do_setupCodec(CodecEngine* _ce, const char* _codecName,
   TRIK_VIDTRANSCODE_CV_Params ceParams;
   memset(&ceParams, 0, sizeof(ceParams));
   ceParams.base.size = sizeof(ceParams);
-  ceParams.base.numOutputStreams = 2;
+  ceParams.base.numOutputStreams = 1;
   ceParams.base.formatInput = do_convertPixelFormat(_ce, _srcImageDesc->m_format);
   ceParams.base.formatOutput[0] = do_convertPixelFormat(_ce, _dstImageDesc->m_format);
   ceParams.base.maxHeightInput = _srcImageDesc->m_height;
